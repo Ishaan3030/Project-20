6bf5c2d9da13e8839529b492083a46163bf8c968
Ishaan3030 <86636860+Ishaan3030@users.noreply.github.com>
2021-07-08 14:12:39 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var big, sleep, brush, gym, eat, drink, 
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
